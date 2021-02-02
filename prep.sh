17bb7112ccf909c6cb93d323740372ba2e2128b6
Kimitoshi Takahshi <ktaka@ccmp.jp>
2021-02-03 04:02:00 +0900

    Add: prep.sh

---
+++ prep.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cat << \EOF > /etc/apt/sources.list.d/testing.list 
+deb http://ftp.jp.debian.org/debian/ testing main non-free
+deb-src http://ftp.jp.debian.org/debian/ testing main non-free
+deb http://ftp.jp.debian.org/debian/ unstable main non-free
+deb-src http://ftp.jp.debian.org/debian/ unstable main non-free
+EOF
+
+cat << \EOF > /etc/apt/preferences
+Package: *
+Pin: release a=stable
+Pin-Priority: 900
+ 
+Package: *
+Pin: release a=testing
+Pin-Priority: 99
+ 
+Package: *
+Pin: release a=unstable
+Pin-Priority: 89
+EOF
+
+aptitude install -t testing debootstrap
+aptitude install -t testing  ubuntu-keyring
+
+
